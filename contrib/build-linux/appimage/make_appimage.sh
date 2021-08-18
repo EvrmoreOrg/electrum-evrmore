97a2ef8c6b97e5c2676d16b6c6b411f6455872db
kralverde <electrum.raven@gmail.com>
2021-08-18 10:58:21 -0400

    tweak appimage build

--- contrib/build-linux/appimage/make_appimage.sh
+++ contrib/build-linux/appimage/make_appimage.sh
@@ -22,7 +22,7 @@ SQUASHFSKIT_COMMIT="ae0d656efa2d0df2fcac795b6823b44462f19386"
 
 
 VERSION=`git describe --tags --dirty --always`
-APPIMAGE="$DISTDIR/electrum-$VERSION-x86_64.AppImage"
+APPIMAGE="$DISTDIR/electrum-ravencoin.$VERSION-x86_64.AppImage"
 
 . "$CONTRIB"/build_tools_util.sh
 

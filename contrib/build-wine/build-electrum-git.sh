e48e2a25ebb3b4471c642cf3fc60c33fd629a015
kralverde <2b2tminecrafter2@gmail.com>
2022-07-25 08:11:32 -0400

    revert versioning

--- contrib/build-wine/build-electrum-git.sh
+++ contrib/build-wine/build-electrum-git.sh
@@ -12,7 +12,7 @@ set -e
 
 pushd $WINEPREFIX/drive_c/electrum
 
-VERSION=0.5.2-develop1
+VERSION=`git describe --tags --dirty --always`
 info "Last commit: $VERSION"
 
 # Load electrum-locale for this release
